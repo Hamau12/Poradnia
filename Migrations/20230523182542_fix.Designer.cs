bcede152b32dd0ed94463c78846c22f75b2191ff
Wojtek Nytko <wojtekn94@gmail.com>
2023-05-23 20:28:38 +0200

    fixt

--- Migrations/20230523182542_fix.Designer.cs
+++ Migrations/20230523182542_fix.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SRP.Models;
 
 namespace Poradnia.Migrations
 {
     [DbContext(typeof(SrpDbContext))]
-    partial class SrpDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230523182542_fix")]
+    partial class fix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
